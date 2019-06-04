3f61102fd984b877cdd8a4844429b79a1c81844c
Garwah Lam <garwahlam@gmail.com>
2019-06-04 22:13:47 +1000

    Functional, need to clean up and log properly

--- index.js
+++ index.js
@@ -3,12 +3,50 @@
 const program = require('commander');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const _ = require('lodash');
 const yaml = require('js-yaml');
 const { exec } = require('child_process');
 
-const path = ".gch.yml";
-let branches = yaml.safeLoad(fs.readFileSync(path, 'utf8'));
+const MAX_BRANCHES = 5;
 
+/**
+* Checkout a given branch.
+*/
+const checkoutBranch = (branchName) => {
+console.info('Checking out:', branchName);
+exec(`git checkout ${branchName}`, (err, stdout, stderr) => {
+	    if (err) {
+	      return;
+	    }
+	    console.info(`${stdout}`);
+	    console.info(`${stderr}`);
+});
+saveBranches(branchName);
+}
+
+/**
+* Load in a list of branch names to switch to.
+*/
+const loadBranches = () => {
+	return yaml.safeLoad(fs.readFileSync(path, 'utf8'));
+}
+
+/**
+* Add a branch to the historical list of branches and dump to file.
+*/
+const saveBranches = (recentBranch) => {
+	branches.unshift(recentBranch);
+	branches = _.uniq(branches).slice(0,MAX_BRANCHES);
+	fs.writeFileSync(path, yaml.safeDump(branches));
+}
+
+const args = program.parse(process.argv).args;
+const path = ".gch.yml";
+let branches = loadBranches().slice(0,MAX_BRANCHES);
+
+if (args.length > 0) {
+	checkoutBranch(args[0]);
+} else {
 inquirer
   .prompt([
     {
@@ -16,9 +54,10 @@ inquirer
       name: 'branches',
       message: 'Checkout which branch?',
       choices: branches,
-    },
+    }
   ])
   .then(answers => {
-    console.info('Answer:', answers.branches);
-    exec(`git checkout ${answers.branches}`);
-  });
+	  checkoutBranch(answers.branches);
+    });
+}
+
